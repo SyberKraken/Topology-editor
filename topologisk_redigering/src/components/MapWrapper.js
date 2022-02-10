4e0cb9c0262139479f2c25191d8d5774f5f6b2cb
Simon Gradin <simgr011@student.liu.se>
2022-02-10 14:19:49 +0100

    WIP pass 2 sprint 1

--- topologisk_redigering/src/components/MapWrapper.js
+++ topologisk_redigering/src/components/MapWrapper.js
@@ -3,22 +3,63 @@ import { Map, View } from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
 import 'ol/ol.css';
+import {Circle, Fill, Stroke, Style} from 'ol/style';
+import Polygon from 'ol/geom/Polygon';
+import { Feature } from 'ol';
+import Geometry from 'ol/geom/Geometry';
+import Point from 'ol/geom/Point';
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
+
 
 //Put vector layer into map. sammanfoga kod från app och wrapper på ett smart sätt 
 
-function MapWrapper( {writeCoordinates} ) {
+function MapWrapper( ) {
     const [map, setMap] = useState();
     const mapElement = useRef();
     const mapRef = useRef();
     mapRef.current = map;
+    const [coordinates, setCoordinates] = useState([]);
+    const [features, setFeatures] = useState([]);
+    const [testCoordinates, setTestCoordinates] = useState([]);
+
+    const saveGEOJson = (points) => {
+        console.log("saving geojson")
+    }
 
     const handleMapClick = (e) => {
         writeCoordinates(e.coordinate)
     }
 
+    const writeCoordinates = (newCoordinates) => {
+    
+        console.log(newCoordinates)
+        setCoordinates(coordinates => [...coordinates,newCoordinates])
+      }
+
+    const drawLayer = () => {
+        setFeatures(features => [...features, new Feature(new Circle(coordinates[0]), 10000)])
+
+        if (features.length >= 4) {
+        console.log("saving!")
+        const vectorSource = new VectorSource({projection: 'EPSG:4326'})
+        vectorSource.addFeatures(features)
+        const vectorLayer = new VectorLayer({source: vectorSource})
+        
+        map.addLayer(vectorLayer)
+        setMap(map)
+      
+        console.log("såsen är klar")
+        
+        saveGEOJson(coordinates)
+        setCoordinates(coordinates => [])
+        }
+        console.log("Features:", features)
+    }
+
     useEffect(() => {
         const initialMap = new Map({
-          target: mapElement.current,
+            target: mapElement.current,
             layers: [
                 new TileLayer({
                     source: new OSM(),
@@ -33,10 +74,15 @@ function MapWrapper( {writeCoordinates} ) {
         setMap(initialMap);
     }, []);
 
+    useEffect(() => {
+        coordinates.length !== 0? drawLayer(coordinates):
+        console.log("FEATURES:", features)
+        }, [coordinates])
+
 
 
     return (
-      <div style={{height:'100vh',width:'100%'}} ref={mapElement} className="map-container" />
+        <div style={{height:'100vh',width:'100%'}} ref={mapElement} className="map-container" />
     );
 }
 
