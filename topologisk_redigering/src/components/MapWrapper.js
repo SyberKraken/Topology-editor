a2b140ae9e820a6f78a96458e80eb0b2c2854883
Daniel <danhu849@student.liu.se>
2022-02-11 10:58:40 +0100

    No delay when drawing lines

--- topologisk_redigering/src/components/MapWrapper.js
+++ topologisk_redigering/src/components/MapWrapper.js
@@ -36,38 +36,41 @@ function MapWrapper( ) {
     const writeCoordinates = (newCoordinates) => {
     
         console.log(newCoordinates)
-        setCoordinates(coordinates => [...coordinates,newCoordinates])
-      }
-      //adds features in he feature list to map and renders to screen
-      const updateMap = () => {
+        setCoordinates(coordinates => [...coordinates, newCoordinates])
+    }
+    //adds features in he feature list to map and renders to screen
+    const updateMap = (newFeature) => {
         const vectorSource = new VectorSource({projection: 'EPSG:4326'})
-        vectorSource.addFeatures(features)
+        vectorSource.addFeature(newFeature)
         const vectorLayer = new VectorLayer({source: vectorSource})
         map.addLayer(vectorLayer)
-      }
-//TODO while drawing a polygon add multiline
+    }
+//TODO while drawing a polygon add multiline (lags one click behind)
 //TODO change form points to coordinates and dra polygon on start= end
 //TODO add precision based on zoom
 //
+
+    const checkPolygonCompleted = (coordinates, precision) => {
+        let startX = coordinates[0][0].toPrecision(precision)
+        let startY = coordinates[0][1].toPrecision(precision)
+
+        let endX = coordinates[coordinates.length-1][0].toPrecision(precision)
+        let endY = coordinates[coordinates.length-1][1].toPrecision(precision)
+        console.log(`${endX}, ${endY}`)
+        return startX === endX && startY === endY && coordinates.length > 2
+    }
+
     const drawLayer = () => {
+
         if (coordinates.length > 1){
             let lineString = new LineString([coordinates[coordinates.length-1],coordinates[coordinates.length-2]])
-            setFeatures(features => [...features, new Feature(lineString)])
-            updateMap()
+            updateMap((new Feature(lineString) ))
         }
-        let startX = coordinates[0][0].toPrecision(1)
-        let startY = coordinates[0][1].toPrecision(1)
-
-        let endX = coordinates[coordinates.length-1][0].toPrecision(1)
-        let endY = coordinates[coordinates.length-1][1].toPrecision(1)
-        console.log(`${endX}, ${endY}`)
+        
+        
         //Runs when a propper polygon is detected
-        if (startX === endX && startY === endY && coordinates.length > 2) {
-            console.log("saving!")
-            updateMap()
-            //setMap(map)
-            console.log("såsen är klar")
-            
+        if (checkPolygonCompleted(coordinates, 1)) {
+            updateMap(/* TODO Update map with polygon */)
             saveGEOJson(coordinates)
             setCoordinates(coordinates => [])
         }
@@ -96,8 +99,8 @@ function MapWrapper( ) {
     }, []);
 
     useEffect(() => {
-        coordinates.length !== 0? drawLayer(coordinates):
-        console.log("FEATURES:", features)
+        coordinates.length !== 0 && drawLayer(coordinates)
+        //console.log("FEATURES: ", features)
         }, [coordinates])
 
     useEffect(() => {
