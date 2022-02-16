e05020d456236b40776331b40b80e2079b1317b7
Edvin Sommermark <edvinsommermark@edvins-mbp.clients.eduroam.liu.se>
2022-02-16 13:59:29 +0100

    reworked polygon drawing. hardcoded function call at line 73, move to appropriate function later

--- topologisk_redigering/src/components/MapWrapper.js
+++ topologisk_redigering/src/components/MapWrapper.js
@@ -3,28 +3,33 @@ import { Map, View } from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
 import 'ol/ol.css';
-import {Circle, Fill, Stroke, Style} from 'ol/style';
-import Polygon from 'ol/geom/Polygon';
-import LineString from 'ol/geom/LineString';
-import { Feature } from 'ol';
-import Geometry from 'ol/geom/Geometry';
-import Point from 'ol/geom/Point';
+import {Fill, Stroke, Style} from 'ol/style';
 import VectorLayer from 'ol/layer/Vector';
 import VectorSource from 'ol/source/Vector';
-import { style } from '@mui/system';
+import Draw from 'ol/interaction/Draw';
 
-
-
-//Put vector layer into map. sammanfoga kod från app och wrapper på ett smart sätt 
+//TODO Save polygon
+//TODO Put vector layer into map. sammanfoga kod från app och wrapper på ett smart sätt 
 
 function MapWrapper( ) {
     const [map, setMap] = useState();
     const mapElement = useRef();
     const mapRef = useRef();
     mapRef.current = map;
-    const [coordinates, setCoordinates] = useState([]);
-    const [features, setFeatures] = useState([]);
-    const [testCoordinates, setTestCoordinates] = useState([]);
+    const [draw, setDraw] = useState()
+
+    const source = new VectorSource({wrapX: false});
+    
+    const vector = new VectorLayer({
+        source: source,
+      });
+
+    const drawPolygon = () => {
+        setDraw(new Draw({
+            source: source,
+            type: "Polygon",
+        }));
+    }
 
     const styles = [
         new Style({
@@ -38,74 +43,25 @@ function MapWrapper( ) {
           }),
     ]
 
-    const saveGEOJson = (points) => {
-        console.log("saving geojson")
-    }
-
     const handleMapClick = (e) => {
-        writeCoordinates(e.coordinate)
     }
 
-    const writeCoordinates = (newCoordinates) => {
-    
-        console.log(newCoordinates)
-        setCoordinates(coordinates => [...coordinates, newCoordinates])
-    }
-    //adds features in he feature list to map and renders to screen
-    const updateMap = (newFeature) => {
+    /*const updateMap = (newFeature) => {
         const vectorSource = new VectorSource({projection: 'EPSG:4326'})
         vectorSource.addFeature(newFeature)
         const vectorLayer = new VectorLayer({source: vectorSource, style: styles, name: "Tag"})
         map.getLayerGroup().getLayers()
         map.addLayer(vectorLayer)
-    }
-//TODO Save polygon
-//TODO change form points to coordinates and draw polygon on start= end
-//TODO add precision based on zoom
-//
-
-    const checkPolygonCompleted = (coordinates, precision) => {
-        let startX = coordinates[0][0].toPrecision(precision)
-        let startY = coordinates[0][1].toPrecision(precision)
-
-        let endX = coordinates[coordinates.length-1][0].toPrecision(precision)
-        let endY = coordinates[coordinates.length-1][1].toPrecision(precision)
-        console.log(`${endX}, ${endY}`)
-        return startX === endX && startY === endY && coordinates.length > 2
-    }
-
-    const drawLayer = () => {
-
-        if (coordinates.length > 1){
-            let lineString = new LineString([coordinates[coordinates.length-1],coordinates[coordinates.length-2]])
-            updateMap((new Feature(lineString) ))
-        }
-        
-        
-
-        //Runs when a propper polygon is detected
-        if (checkPolygonCompleted(coordinates, 1)) {
-            coordinates[coordinates.length - 1] = coordinates[0]
-            const polyT = new Feature(new Polygon([coordinates]) )
-            console.log(polyT)
-            updateMap(polyT/* TODO Update map with polygon */)
-            saveGEOJson(coordinates)
-            setCoordinates(coordinates => [])
-        }
-        console.log("Features:", features)
-    }
-
-    const drawFeatures = () => {
-       
-    }
+    }*/
 
     useEffect(() => {
         const initialMap = new Map({
             target: mapElement.current,
             layers: [
-                new TileLayer({
+                new TileLayer({ 
                     source: new OSM(),
                 }),
+                vector
             ],
             view: new View({
                 center: [1800000, 1800000],
@@ -114,18 +70,15 @@ function MapWrapper( ) {
         });
         initialMap.on('click', handleMapClick)
         setMap(initialMap);
+        drawPolygon();  //TODO: move to button interaction
+
     }, []);
 
     useEffect(() => {
-        coordinates.length !== 0 && drawLayer(coordinates)
-        //console.log("FEATURES: ", features)
-        }, [coordinates])
-
-    useEffect(() => {
-        drawFeatures()
-        
-        }, [features])
-
+        if (map) {
+             map.addInteraction(draw) 
+            }
+        }, [draw])
 
     return (
         <div style={{height:'100vh',width:'100%'}} ref={mapElement} className="map-container" />
