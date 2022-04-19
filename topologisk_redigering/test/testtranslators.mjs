27ba0f2d65a9decc2965ebfc3762d1016a9dd1e3
Josefin Bodin <josbo110@student.liu.se>
2022-04-19 11:19:22 +0200

    tests for full geo <-> featureCollection added

--- topologisk_redigering/test/testtranslators.mjs
+++ topologisk_redigering/test/testtranslators.mjs
@@ -1,10 +1,12 @@
 import { olFeatures2GeoJsonFeatureCollection, geoJsonFeatureCollection2olFeatures,geoJsonFeature2JstsGeometry,
     jstsGeometry2GeoJsonFeature, jstsGeometries2GeoJsonFeatureCollection, geoJsonFeatureCollection2JstsGeometries,
-    geoJsonFeatureCollection2FullGeoJSON } from "../src/Translations/Translators.mjs";
+    geoJsonFeatureCollection2FullGeoJSON, 
+    fullGeoJson2GeoJsonFeatureCollection} from "../src/translation/Translators.mjs";
 import test from 'tape'
 import GeoJSON from "ol/format/GeoJSON.js";
 
-const featureCollection = {
+const featureCollection = () => {
+    return {
 "type": "FeatureCollection",
 "features": [{
 "type":"Feature",
@@ -53,29 +55,108 @@ const featureCollection = {
 },
 "properties": null
 }]
-}
+}}
 
-const feature = {
-"type":"Feature",
-"properties": null,
-"geometry": {
-"type": "Polygon",
-"coordinates": [
-[
-    [0, 1],
-    [0, 2],
-    [2, 2],
-    [2, 1],
-    [0, 1]
-]
-]
-}
-}
+const fullGeoJson = () => {
+    return {
+    "type": "FeatureCollection",
+    "features": [{
+    "type":"Feature",
+    "geometry": {
+    "type": "Polygon",
+    "coordinates": [
+        [
+            [0, 0],
+            [0, 1],
+            [1, 1],
+            [1, 0],
+            [0, 0]
+        ]
+    ]
+    },
+    "properties": null
+    
+    },{
+    "type":"Feature",
+    "geometry": {
+    "type": "Polygon",
+    "coordinates": [
+        [
+            [1, 0],
+            [1, 1],
+            [2, 1],
+            [2, 0],
+            [1, 0]
+        ]
+    ]
+    },
+    "properties": null
+    },{
+    "type":"Feature",
+    "geometry": {
+        "type": "Polygon",
+        "coordinates": [
+        [
+            [0, 1],
+            [0, 2],
+            [2, 2],
+            [2, 1],
+            [0, 1]
+        ]
+        ]
+    },
+    "properties": null
+    }],
+    "crs": {
+        "type": "name",
+        "properties": {
+            "name": "EPSG:3006"
+            }
+        }
+    }}
 
-const olFeatureList = new GeoJSON().readFeatures(featureCollection)
+const feature = () => {
+ return {
+    "type":"Feature",
+    "properties": null,
+    "geometry": {
+    "type": "Polygon",
+    "coordinates": [
+    [
+        [0, 1],
+        [0, 2],
+        [2, 2],
+        [2, 1],
+        [0, 1]
+    ]
+    ]
+    }
+    }
+} 
 
+const olFeatureList = new GeoJSON().readFeatures(featureCollection())
+
+
+
+
+test('Convert a full geoJson to a featureCollection', function(t){
+    let actual = fullGeoJson2GeoJsonFeatureCollection(fullGeoJson())
+    let expected = featureCollection()
+    t.deepEqual(actual, expected)
+    t.end()
+})
+
+test('Convert a featureCollection to a full geoJson', function(t){
+    let actual = geoJsonFeatureCollection2FullGeoJSON(featureCollection())
+    let expected = fullGeoJson()
+    t.deepEqual(actual, expected)
+    t.end()
+})
 
 test('Convert a list of OL features to a FeatureCollection', function(t) {
-t.deepEqual(olFeatures2GeoJsonFeatureCollection(olFeatureList), featureCollection)
-t.end()
+    let actual = olFeatures2GeoJsonFeatureCollection(olFeatureList)
+    let expected = featureCollection()
+    t.deepEqual(actual, expected )
+    t.end()
 })
+
