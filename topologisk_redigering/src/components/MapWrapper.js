aa3d75e705c7bd36e330e33c9fd7f36329bb4e7d
Edvin Sommermark <edvni392@student.liu.se>
2022-05-10 11:56:57 +0200

    added comments to mapwrapper

--- topologisk_redigering/src/components/MapWrapper.js
+++ topologisk_redigering/src/components/MapWrapper.js
@@ -25,9 +25,18 @@ import SaveIcon from '@mui/icons-material/Save';
 import { Button } from '@mui/material';
 import QuestionMarkIcon from '@mui/icons-material/QuestionMark';
 import { Polygon } from 'ol/geom';
+import { DoubleClickZoom } from 'ol/interaction';
 
+/*
+MapWrapper contains the on screen map and runs functionality according to user interaction.
+The user draws polygons by placing their nodes on the map with left click. 
+Polygons are removed when double clicked.
+Polygon borders can be modified by dragging an edge or node.
+Polygons can be merged by clicking on two polygons in succession, if they are adjececent by more than a single coordinate.
+*/
 
 function MapWrapper() {
+    //Common data used for most operations
     const [map, setMap] = useState();
     let clickHandlerState = 'NONE';
     const mapElement = useRef();
@@ -35,21 +44,24 @@ function MapWrapper() {
     mapRef.current = map;
     let currentClickedPolygon = null
     let previousClickedPolygon = null
+
+    //data used for later creating a tilegrid, which stops the map from trying to render data outside of the map's borders.
     const projection = getProjection('EPSG:3857');
     const projectionExtent = projection.getExtent();
     const size = getWidth(projectionExtent) / 256;
     const resolutions = new Array(19);
     const matrixIds = new Array(19);
-
     for (let z = 0; z < 19; ++z) {
         //generate resolutions and matrixIds arrays for this WMTS
-        resolutions[z] = size / Math.pow(2, z);
+        resolutions[z] =size / Math.pow(2, z);
         matrixIds[z] = z;
-    }
+    } 
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// -------------------------------------------Create map-------------------------------------------------
+    
     const attributions = "Click screen to draw, double click polygon to remove and to merge click two adjacent polygons"
     
+    //Tilegrid
     const OUTER_SWEDEN_EXTENT = [-1200000, 4700000, 2600000, 8500000];
     const wmts_3006_resolutions = [4096.0, 2048.0, 1024.0, 512.0, 256.0, 128.0, 64.0, 32.0, 16.0, 8.0];
     const wmts_3006_matrixIds = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];   
@@ -61,6 +73,7 @@ function MapWrapper() {
         matrixIds: wmts_3006_matrixIds
     });
 
+    //Loads map of sweden from Lantmäteriest, with the created tilegrid.
     const swedenMapLayer = new TileLayer({
         source: new WMTS({
             attributions: attributions,
@@ -79,6 +92,7 @@ function MapWrapper() {
         wrapX: true,
     })
 
+    //Loads geoJson data from server via url
     const source = new VectorSource({
         wrapX: false,
         /* url: "http://localhost:4000/file1",   */
@@ -104,6 +118,8 @@ function MapWrapper() {
 
 });
 
+//------------------------------ Map Actions ---------------------------------------
+
     const polygonLayer = new VectorLayer({
         source: source,
         style: defaultStyle
@@ -113,9 +129,11 @@ function MapWrapper() {
         source: source, 
         hitDetection: true
     })
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     
-    //fixes overlaps for the latest polygon added to map
+    /*  
+    Removes parts of the most recently added polygon on the map that overlap with 
+        existing polygons. on the intersection points, the new polygon has nodes added.
+    */
     const cleanUserInput = (map, modifiedFeatures=1) => {
         console.log("cleanuserinput, map has this many features:")
         
@@ -138,6 +156,8 @@ function MapWrapper() {
         projection: "EPSG:3006",
     })
 
+
+    // To be implemented... 
     const cleanself = (evt) => {
         //alt 1: run this on all polygons who change. Stop propagation to hinder repeated firing.
         //alt 2: getPointClosestToPixel, get all features who share pixel, run cleanuserinput/handleintersections on all of them.
@@ -145,6 +165,9 @@ function MapWrapper() {
         //console.log("Feature change event provides:", evt.target)
     }
 
+    /*
+        sets initial values and interactions for the Openlayers Map. 
+    */
     useEffect(() => {
         const initialMap = new Map({
             controls: defaultControls().extend([mousePositionControl]),
@@ -171,24 +194,35 @@ function MapWrapper() {
         initialMap.on('click', onMapClickGetPixel) // can I get closest pixel from here?
         initialMap.addInteraction(modify)
         modify.on('modifyend', handleModifyend)
+
+        // remove DoubleClickZoom interaction from the map
+        initialMap.getInteractions().getArray().forEach(function(interaction) {
+            if (interaction instanceof DoubleClickZoom) {
+                initialMap.removeInteraction(interaction);
+                console.log(DoubleClickZoom.name)
+            }
+        }
+        )
+
         setMap(initialMap)
     }, []);
 
 
-    /* Contextual clickhandler, different actions depending on if you click on a polygon or somewhere on the map */
+     
+    //Contextual clickhandler, different actions depending on if you click on a polygon or somewhere on the map 
     const onMapClickGetPixel = (event) => {
-        
-        /* Check if clicked on an existing polygon */
+        //Check if clicked on an existing polygon 
         if (isPolygon(event.map, event.pixel)){
             currentClickedPolygon = getPolygon(event.map, event.pixel) 
             currentClickedPolygon.setStyle(selectedStyle)
             if(previousClickedPolygon != null){
                 previousClickedPolygon.setStyle(defaultStyle)
                 if(currentClickedPolygon !== -1){
+                    //merge if this click occurs directly after a click on another polygon
                     if(currentClickedPolygon !== previousClickedPolygon){
                         merge(event.map)
                     }
-                    
+                    //delete if this click is on the same polygon as the last click.
                     if (currentClickedPolygon === previousClickedPolygon) {
                         deletePolygon(event.map, currentClickedPolygon)
                     }
@@ -198,8 +232,7 @@ function MapWrapper() {
         } 
         else {
             if(clickHandlerState === 'DRAWEND') {
-                //console.log("Running checks because polygon is finished drawing")
-                //unkink the drawn polygon HERE
+                //TODO: MAYBE unkink the drawn polygon HERE
                 cleanUserInput(event.map)
                 clickHandlerState = 'NONE'
             }
@@ -210,12 +243,11 @@ function MapWrapper() {
                     clickHandlerState = 'DRAWEND'
                     event.map.getInteractions().getArray().pop()
                     event.map.getInteractions().getArray().pop()
-
                 }) 
             }
         }
     }
-
+    //merges the two most recently clicked polygons when called
     const merge = (map) => {
         const featureList = olFeatures2GeoJsonFeatureCollection(getFeatureList(map))
         let newPoly = handleMerge(olFeature2geoJsonFeature(currentClickedPolygon), olFeature2geoJsonFeature(previousClickedPolygon),featureList)
@@ -224,13 +256,13 @@ function MapWrapper() {
             let OlPoly = (new GeoJSON()).readFeature(newPoly)
             deletePolygon(map, currentClickedPolygon)
             deletePolygon(map, previousClickedPolygon)
-            getSource(map).addFeature(OlPoly)
-                
+            getSource(map).addFeature(OlPoly)   
         }else{
             console.log("didnt find the poly in the list")
         }
     }
 
+    //interprets newly drawn polygon and modifies it to not break topology rules.
     const handleDrawend = (evt, map) => {
         const mapSource = map.getLayers().getArray()[1].getSource()
 
@@ -244,11 +276,10 @@ function MapWrapper() {
         
         if (!valid)
         {
-            // if not valid unkink
-            // return geoJsonFeatureCollection
+            //if not valid unkink: return geoJsonFeatureCollection
             const unkinkedCollection = unkink(olFeature2geoJsonFeature(evt.feature))
             
-            // check intersection and add unkinked polys to the source
+            //check intersection and add unkinked polys to the source
             const olFeatures = geoJsonFeatureCollection2olFeatures(unkinkedCollection)
             mapSource.addFeatures(olFeatures)
             cleanUserInput(map)
@@ -261,6 +292,7 @@ function MapWrapper() {
         }
     }
 
+    //interprets newly modified polygon and modifies it to not break topology rules.
     const handleModifyend = (event) => {
         console.log("modifyend event.target: ", event.features.getArray())
         console.log("modifyend event.target.length: ", event.features.getArray().length)
@@ -290,12 +322,6 @@ function MapWrapper() {
             source2.addFeature(feature)
             cleanUserInput(event.target.map_)
         })
-        
-        
-        // erros to cry about
-            // unable to assign hole to a shell wut??
-            // side location conflict
-            // found non-noded intersection 
     }
 
     const handleNewPoly = (evt) => {
